0af8cbbe73d01669449da58f1d28f12118f17f3e
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-05-17 14:41:31 +0200

    adding milestone 2 & minimal styles

--- index.php
+++ index.php
@@ -4,21 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
     <title>BadWords</title>
 </head>
 <body>
 
 <?php
+// TEXT
 $text = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum cupiditate totam enim id libero sequi temporibus veniam, consectetur, obcaecati recusandae quisquam voluptatum officiis labore repellat non itaque voluptatibus laboriosam sed.'
 ?>
 
-<header>
-    <h1>Welcome PHP</h1>
+<?php
+// BADWORD
+$bad = $_GET['badword'];
+$replaced = str_replace($bad, '***', $text);
+?>
+
+<header class="container">
+    <h1 class="title">Welcome <mark>PHP</mark></h1>
 </header>
 
 <main>
-    <p><?php echo $text ?></p>
-    <p>The length of the previous paragraph is <strong><?php echo strlen($text) ?></strong></p>
+    <div class="container">
+        <p><?php echo $text ?></p>
+        <p class="length">The length of the previous paragraph is: <strong><?php echo strlen($text) ?></strong></p>
+    </div>
+    <div class="container">
+        <p class="censor">Here is a censored version of the previous paragraph.</p>
+        <p><?php echo $replaced ?></p>
+    </div>
 </main>
+
 </body>
 </html>
